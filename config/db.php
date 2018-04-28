a59474582ba8a2e2257672a441fe279e90325c80
Tiago Farias <tiago.farias.poa@gmail.com>
2018-04-28 14:35:05 -0300

    add migration: create_category_costs

--- config/db.php
+++ config/db.php
@@ -5,7 +5,7 @@ return [
         'driver' => 'mysql',
         'host' => 'localhost',
         'database' => 'financas',
-        'user' => 'root',
+        'username' => 'root',
         'password' => 'root',
         'charset' => 'utf8',
         'collation' => 'utf8_unicode_ci'
