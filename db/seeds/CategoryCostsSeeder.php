e8619f87096fab7afbff454f482e778addc3a055
Tiago Farias <tiago.farias.poa@gmail.com>
2018-04-28 15:37:33 -0300

    add fzaninotto/faker

--- db/seeds/CategoryCostsSeeder.php
+++ db/seeds/CategoryCostsSeeder.php
@@ -14,20 +14,20 @@ class CategoryCostsSeeder extends AbstractSeed
      */
     public function run()
     {
+        $faker = \Faker\Factory::create('pt_BR');
+
         $categoryCosts = $this->table('category_costs');
-        $categoryCosts->insert(
-            [
-                [
-                    'name' => 'Category 1',
-                    'created_at' => date('Y-m-d H:i:s'),
-                    'updated_at' => date('Y-m-d H:i:s'),
-                ],
-                [
-                    'name' => 'Category 2',
-                    'created_at' => date('Y-m-d H:i:s'),
-                    'updated_at' => date('Y-m-d H:i:s'),
-                ]
-            ]
-        )->save();
+
+        $data = [];
+        foreach( range(1,10) as $value )
+        {
+            $data[] = [
+                'name' => $faker->name(),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ];
+        }
+
+        $categoryCosts->insert( $data )->save();
     }
 }
