aeb2db6ad039d46d27e0e9c96227701f4a3a2bd3
Tiago Farias <tiago.farias.poa@gmail.com>
2018-04-28 14:02:10 -0300

    add robmorgan/phinx:0.9.2

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'development' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'financas',
+        'user' => 'root',
+        'password' => 'root',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci'
+    ]
+];
