7b214195b85b7709b20109f0ca7f6c195ac26601
Tiago Farias <tiago.farias.poa@gmail.com>
2018-04-28 20:22:24 -0300

    add configurando rotas

--- src/Plugins/RoutePlugin.php
+++ src/Plugins/RoutePlugin.php
@@ -1,23 +1,42 @@
 <?php
-
+declare(strict_types=1);
 namespace Fin\Plugins;
 
 use Aura\Router\RouterContainer;
 use Fin\ServiceContainerInterface;
+use Psr\Http\Message\RequestInterface;
+use Interop\Container\Containerinterface;
 
 class RoutePlugin
 {
     public function register(ServiceContainerInterface $container)
     {
         $routerContainer = new RouterContainer();
-        // Registrar as rotas da aplicação
+        // Registra as rotas da aplicação
         $map = $routerContainer->getMap();
-        // Identificar a rota que está sendo acessada
+        // Identifica a rota que está sendo acessada
         $matcher = $routerContainer->getMatcher();
-        // Gerar links com base nas rotas registradas
+        // Gera links com base nas rotas registradas
         $generator = $routerContainer->getGenerator();
+
+        $request = $this->getRequest();
+
         $container->add('routing', $map);
         $container->add('routing.matcher', $matcher);
         $container->add('routing.generator', $generator);
+        $container->add(RequestInterface::class, $request);
+
+        $container->addLazy('route', function( ContainerInterface $container){
+            $matcher = $container->get('routing.matcher');
+            $request = $container->get(RequestInterface::class);
+            return $matcher->match( $request );
+        });
+    }
+
+    protected function getRequest() : RequestInterface
+    {
+        return ServerRequestFactory::fromGlobals(
+            $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+        );
     }
 }
