1d4797dbdf8316f2b93d7affd1eea281156d06c4
Tiago Farias <tiago.farias.poa@gmail.com>
2018-04-29 19:23:19 -0300

    add testando permissao a area administrativa

--- src/controllers/auth.php
+++ src/controllers/auth.php
@@ -28,3 +28,17 @@ $app->post('/login', function(\Psr\Http\Message\ServerRequestInterface $request)
 
     return $app->route('category-costs.list');
 }, 'auth.login');
+
+$app->before(function() use($app){
+
+    $route = $app->service('route');
+    $auth = $app->service('auth');
+    $routeWhiteList = [
+        'auth.show_login_form',
+        'auth.login'
+    ];
+
+    if( !in_array($route->name, $routeWhiteList) && !$auth->check() ){
+        return $app->route('auth.show_login_form');
+    }
+});
